2115190eb3308f191798e23d9a73a7864218148f
Seth Madlon-Kay <seth.madlonkay@gmail.com>
2015-08-13 16:36:31 -0400

    It's alive! Requires Julia 0.4. 3x faster than MATLAB.

--- BSFG_core.jl
+++ BSFG_core.jl
@@ -1,14 +1,14 @@
-@debug function BSFG_sampler(X,Y,A,Z,burn,thin,iter,eps,priors)
+function BSFG_sampler(X,Y,A,Z,burn,thin,iter,priors)
 
   #get dimensions
   r=size(Z,1);  #biological replicates
   b=size(X,1);  #
   n,p=size(Y);
-  k = priors["k_init"];
-  if isempty(priors["k_max"])
-    priors["k_max"] = p;
+  k = priors[:k_init];
+  if priors[:k_max] <= 0
+    priors[:k_max] = p;
   end
-  kmax = priors["k_max"];
+  kmax = priors[:k_max];
 
   data_norm!(Y,X,p,b);
   Yt = Y';
@@ -22,7 +22,7 @@
   F,G,B,Rps,P = initialize(n,p,r,b,k,nsave,priors);
 
   #precalculate covariance quantities for fast inversion
-  ZAZvec,ZAZval,DA_Q,DA_S1,DA_S2,QtD,ZA_Q,ZA_S1,ZA_S2 = precalc(X,Z,A,priors["B_Sinv"]);
+  ZAZvec,ZAZval,DA_Q,DA_S1,DA_S2,QtD,ZA_Q,ZA_S1,ZA_S2 = precalc(X,Z,A,priors[:B_Sinv]);
 
   #find missing data points
   missloc = isnan(Y);
@@ -31,62 +31,67 @@
   misssub = ind2sub((n,p),missloc);
 
   #preallocate some containers
-  shells = preallocate(n,p,r,priors["h2div"],b,kmax);
-  krop!(F,G,shells,k);
+  krop!(F,G,k);
   Lam2 = Array(Float64,(p,kmax));
-  Lam2_sub = sub(Lam2,:,1:k);
-  resid = shells["pbyn"];
+  resid = Array(Float64,(p,n));
 
+  tic();
   for i in 1:niter
-
     #impute missing phenotypes
     if missing
-      sample_missing!(Yt,shells["Yt"],B,X,k,missloc,misssub);
+      sample_missing!(Yt,F[:Lambda_sub],F[:scores_sub],B,X,k,missloc,misssub);
     end
 
     #sample Lambda
-    sample_Lambda!(F,G["ps"],Rps,X,B,Yt,ZAZvec,ZAZval,k,p,shells);
-    copy!(Lam2_sub,F["Lambda_sub"]);
-    pow!(Lam2,2);
-
+    sample_Lambda!(F[:Lambda_sub],F[:scores_sub],F[:Plam_sub],G[:ps],Rps,X,B,Yt,ZAZvec,ZAZval,k,p,n);
+    Lam2_sub = view(Lam2,:,1:k);
+    @into! Lam2_sub = F[:Lambda_sub].*F[:Lambda_sub];
     #sample B and D
-    sample_BD!(B,G,F,Rps,Yt,QtD,DA_Q,DA_S1,DA_S2,n,b,p,shells);
+    sample_BD!(B,G[:d],G[:ps],F[:Lambda_sub],F[:scores_sub],Rps,Yt,QtD,DA_Q,DA_S1,DA_S2,n,b,p);
 
     #sample heritabilities of factors
-    sample_h2!(F,ZAZvec,ZAZval,r,k,n,priors["h2div"],shells);
+    sample_h2!(F[:h2],F[:scores_sub],ZAZvec,ZAZval,r,k,n,priors[:h2div]);
 
     #sample genetic factors
-    sample_U!(G["U"],Z,F,ZA_Q,ZA_S1,ZA_S2,k,r,shells);
+    sample_U!(G[:U_sub],F[:h2_sub],F[:scores_sub],Z,ZA_Q,ZA_S1,ZA_S2,k,r);
 
     @into! resid = B*X;
-    BLAS.gemm!('N','N',1.0,G["d"],Z,1.0,resid);
-    add!(negate!(resid),Yt);
+    BLAS.gemm!('N','N',1.0,G[:d],Z,1.0,resid);
+    NumericExtensions.add!(NumericExtensions.negate!(resid),Yt);
     #sample factor scores
-    sample_scores!(F,G,Z,resid,Rps,k,n,shells);
+    sample_scores!(F[:scores_sub],F[:Lambda_sub],F[:h2_sub],G[:U_sub],Z,resid,Rps,k,r,n);
+    BLAS.gemm!('N','N',-1.0,F[:Lambda_sub],F[:scores_sub],1.0,resid);
 
-    BLAS.gemm!('N','N',-1.0,F["Lambda_sub"],F["scores_sub"],1.0,resid);
     #update precisions
-    sample_precisions!(F,G,Rps,Lam2_sub,resid,k,p,n,r,priors,shells);
+    sample_precisions!(F[:psijh_sub],G[:ps],Rps,F[:tauh_sub],G[:d],
+                       Lam2_sub,resid,k,p,n,r,priors[:df],priors[:as],priors[:bs]);
 
     #update factor-specific shinkage
-    sample_delta!(F,Lam2_sub,priors,k,p,shells);
+    sample_delta!(F[:delta_sub],F[:tauh_sub],F[:psijh_sub],Lam2_sub,
+                  k,p,priors[:ad1],priors[:bd1],priors[:ad2],priors[:bd2]);
 
     #update total precision
-    @into! F["Plam"] = F["psijh"] .* Base.transpose!(shells["kt"],F["tauh"]);
+    broadcast!(*,F[:Plam_sub],F[:psijh_sub],F[:tauh_sub]');
 
     #update number of factors
-    update_k!(F,G,Z,priors,i,p,k,r,eps,1.0,shells);
-    if k != F["k"]
-      k = F["k"];
-      krop!(F,G,shells,k);
-    end
+    k = update_k!(F,G,Z,priors,i,p,k,r,n);
 
     #store samples
     nsamp = findin(saveiter,i);
     if ~isempty(nsamp)
-      store_posterior!(P,F,G,Rps,B,nsamp);
+      P[:Lambda][:,:,nsamp] = F[:Lambda];
+      P[:U][:,:,nsamp] = G[:U];
+      P[:delta][:,nsamp] = F[:delta];
+      P[:h2][:,nsamp] = F[:h2];
+      P[:Gps][:,nsamp] = G[:ps];
+      P[:Rps][:,nsamp] = Rps;
+      P[:B][:,:,nsamp] = B;
+      print(i,"/",niter,"; ");
+      print(k," factors\r")
     end
 
   end
+  print("\n")
+  toc();
   return P,priors
 end
--- BSFG_pre.jl
+++ BSFG_pre.jl
@@ -11,83 +11,57 @@ function data_norm!(Y,X,p,b)
   end
 end
 
-function preallocate(n,p,r,s,b,kmax)
-  shells = Dict{ASCIIString,Array}();
-  shells["Y"] = Array(Float64,(n,p));
-  shells["Yt"] = Array(Float64,(p,n));
-  shells["pbyk"] = Array(Float64,(p,kmax));
-  shells["kbyp"] = Array(Float64,(kmax,p));
-  shells["nbyp"] = Array(Float64,(n,p));
-  shells["pbyn"] = Array(Float64,(p,n));
-  shells["kbyn"] = Array(Float64,(kmax,n));
-  shells["kbyn2"] = Array(Float64,(kmax,n));
-  shells["kbys"] = Array(Float64,(kmax,s));
-  shells["bnbyp"] = Array(Float64,(b+n,p));
-  shells["rbyk"] = Array(Float64,(r,kmax));
-  shells["rbyk2"] = Array(Float64,(r,kmax));
-  shells["kbyk"] = Array(Float64,(kmax,kmax));
-  shells["kbyr"] = Array(Float64,(kmax,r));
-  shells["r"] = Array(Float64,r);
-  shells["r2"] = Array(Float64,r);
-  shells["p"] = Array(Float64,p);
-  shells["k"] = Array(Float64,kmax);
-  shells["k2"] = Array(Float64,kmax);
-  shells["kt"] = Array(Float64,1,kmax);
-  shells["st"] = Array(Float64,1,s);
-  shells["n"] = Array(Float64,n);
-  shells["nt"] = Array(Float64,1,n);
-
-  return shells
-end
-
 function initialize(n,p,r,b,k,nsave,priors)
-  Rps = rand(Gamma(priors["as"],1/priors["bs"]),p);
-  kmax = priors["k_max"];
+  Rps = rand(Gamma(priors[:as],1/priors[:bs]),p);
+  kmax = priors[:k_max];
   #genetic effects. This structure holds information about latent genetic
   #effects. U is latent genetic effects on factor traits. d is genetic
   #effects on residuals of the factor traits. Plus prior hyperparameters for
   #G effect precisions
-  G = Dict{ASCIIString,Any}();
-  G["ps"] = rand(Gamma(priors["as"],1/priors["bs"]),p);
-  G["U"] = fill(NaN,(kmax,r));
-  G["d"] = Array(Float64,(p,r));
+  G = Dict{Symbol,Union{Array{Float64,1},Array{Float64,2},
+                        ArrayViews.StridedView{Float64,2,1,Array{Float64,2}}}}();
+  G[:ps] = rand(Gamma(priors[:as],1/priors[:bs]),p);
+  G[:U] = fill(NaN,(kmax,r));
+  G[:d] = Array(Float64,(p,r));
   for j in 1:p
-    G["d"][j,:] = rand(Normal(0, 1/sqrt(G["ps"][j]) ),r);
+    G[:d][j,:] = rand(Normal(0, 1/sqrt(G[:ps][j]) ),r);
   end
 
   B = rand(Normal(),(p,b));
 
-  F = Dict{ASCIIString,Any}();
-  F["k"]=priors["k_init"];
-  F["psijh"] = fill(NaN,(p,kmax));
-  F["psijh"][:,1:k] = rand(Gamma(priors["df"]/2,2/priors["df"]),(p,k));    #individual loadings precisions
-  F["delta"] = fill(NaN,kmax);
-  F["delta"][1] = rand(Gamma(priors["ad1"]+10,1/priors["bd1"]));
-  rand!(Gamma(priors["ad2"],1/priors["bd2"]),sub(F["delta"],2:k));  #components of tauh
-  F["tauh"] = cumprod(F["delta"]);        #extra shrinkage of each loading column
-  F["Plam"] = F["psijh"] .* F["tauh"]';   #total precision of each loading
-  F["Lambda"] = fill(NaN,(p,kmax));
-  rand!(Normal(),sub(F["Lambda"],:,1:kmax));
-  @in1! F["Lambda"] ./ sqrt(F["Plam"]);   #factor loadings
-  F["h2"] = fill(NaN,kmax);
-  F["h2"][1:k] = rand(k);                   #factor heritability
+  F = Dict{Symbol,Union{Array{Float64,1},Array{Float64,2},
+                        ArrayViews.ContiguousView{Float64,2,Array{Float64,2}},
+                        ArrayViews.ContiguousView{Float64,1,Array{Float64,1}},
+                        ArrayViews.StridedView{Float64,2,1,Array{Float64,2}}}}();
+  F[:psijh] = fill(NaN,(p,kmax));
+  F[:psijh][:,1:k] = rand(Gamma(priors[:df]/2,2/priors[:df]),(p,k));    #individual loadings precisions
+  F[:delta] = fill(NaN,kmax);
+  F[:delta][1] = rand(Gamma(priors[:ad1]+10,1/priors[:bd1]));
+  rand!(Gamma(priors[:ad2],1/priors[:bd2]),sub(F[:delta],2:k));  #components of tauh
+  F[:tauh] = cumprod(F[:delta]);        #extra shrinkage of each loading column
+  F[:Plam] = F[:psijh] .* F[:tauh]';   #total precision of each loading
+  F[:Lambda] = fill(NaN,(p,kmax));
+  rand!(Normal(),sub(F[:Lambda],:,1:kmax));
+  @in1! F[:Lambda] ./ sqrt(F[:Plam]);   #factor loadings
+  F[:h2] = fill(NaN,kmax);
+  F[:h2][1:k] = rand(k);                   #factor heritability
   for j in 1:k
-    rand!(Normal(0, sqrt(F["h2"][j]) ),sub(G["U"],j,:));
+    rand!(Normal(0, sqrt(F[:h2][j]) ),sub(G[:U],j,:));
   end
-  F["scores"] = G["U"]*Z;
+  F[:scores] = G[:U]*Z;
   for j in 1:k
-    F["scores"][j,:] += rand(Normal(0,1-F["h2"][j]),(1,n)); #initialize factor scores
+    F[:scores][j,:] += rand(Normal(0,1-F[:h2][j]),(1,n)); #initialize factor scores
   end
 
   #Initialize posteior samples
-  P = Dict{ASCIIString,Array{Float64}}();
-  P["Lambda"] = Array(Float64,(p,kmax,nsave));
-  P["Gps"] = Array(Float64,(p,nsave));
-  P["Rps"] = Array(Float64,(p,nsave));
-  P["B"] = Array(Float64,(p,b,nsave));
-  P["U"] = Array(Float64,(kmax,r,nsave));
-  P["delta"] = Array(Float64,(kmax,nsave));
-  P["h2"] = Array(Float64,(kmax,nsave));
+  P = Dict{Symbol,Union{Array{Float64,2},Array{Float64,3}}}();
+  P[:Lambda] = Array(Float64,(p,kmax,nsave));
+  P[:Gps] = Array(Float64,(p,nsave));
+  P[:Rps] = Array(Float64,(p,nsave));
+  P[:B] = Array(Float64,(p,b,nsave));
+  P[:U] = Array(Float64,(kmax,r,nsave));
+  P[:delta] = Array(Float64,(kmax,nsave));
+  P[:h2] = Array(Float64,(kmax,nsave));
 
   return F,G,B,Rps,P
 
@@ -107,10 +81,10 @@ function precalc(X,Z,A,B_Sinv)
   Design = vcat(X,Z);
   Dcov = Design*Design';
   priorPs = full(blkdiag(sparse(B_Sinv),sparse(Ainv)));
-  svdout = svdfact( covfact(priorPs), covfact(Dcov) );
-  DA_Q = inv( (svdout[:R0] * svdout[:Q]')' )';
-  DA_S1 = diag( svdout[:D1]'*svdout[:D1] );
-  DA_S2 = diag( svdout[:D2]'*svdout[:D2] );
+  foo,foo,q,S1,S2 = mxcall(:gsvd,5,covfact(priorPs),covfact(Dcov));
+  DA_Q = inv(q)';
+  DA_S1 = diag( S1'S1 );
+  DA_S2 = diag( S2'S2 );
   QtD = DA_Q' * Design;
 
   #G effect variances of factor traits
@@ -120,37 +94,24 @@ function precalc(X,Z,A,B_Sinv)
   #similar to fixed effects + random effects 1 above, but no fixed effects.
 
   ZZt = Z*Z';
-  svdout = svdfact( covfact(ZZt), covfact(Ainv) );
-  ZA_Q = inv( (svdout[:R0] * svdout[:Q]')' )';
-  ZA_S1 = diag( svdout[:D1]'*svdout[:D1] );
-  ZA_S2 = diag( svdout[:D2]'*svdout[:D2] );
+  foo,foo,q,S1,S2 = mxcall(:gsvd,5,covfact(ZZt),covfact(Ainv));
+  ZA_Q = inv(q)';
+  ZA_S1 = diag( S1'S1 );
+  ZA_S2 = diag( S2'S2 );
 
   return ZAZvec,ZAZval,DA_Q,DA_S1,DA_S2,QtD,ZA_Q,ZA_S1,ZA_S2
 end
 
-function krop!(F,G,shells,k)
-  G["U_sub"] = sub(G["U"],1:k,:);
+function krop!(F,G,k)
+  G[:U_sub] = view(G[:U],1:k,:);
 
-  F["psijh_sub"] = sub(F["psijh"],:,1:k);
-  F["delta_sub"] = sub(F["delta"],1:k);
-  F["tauh_sub"] = sub(F["tauh"],1:k);
-  F["Plam_sub"] = sub(F["Plam"],:,1:k);
-  F["Lambda_sub"] = sub(F["Lambda"],:,1:k);
-  F["h2_sub"] = sub(F["h2"],1:k);
-  F["scores_sub"] = sub(F["scores"],1:k,:);
-
-  shells["pbyk_sub"] = sub(shells["pbyk"],:,1:k);
-  shells["kbyp_sub"] = sub(shells["kbyp"],1:k,:);
-  shells["kbyn_sub"] = sub(shells["kbyn"],1:k,:);
-  shells["kbyn2_sub"] = sub(shells["kbyn2"],1:k,:);
-  shells["rbyk_sub"] = sub(shells["rbyk"],:,1:k);
-  shells["rbyk2_sub"] = sub(shells["rbyk2"],:,1:k);
-  shells["kbyk_sub"] = sub(shells["kbyk"],1:k,1:k);
-  shells["kbys_sub"] = sub(shells["kbys"],1:k,:);
-  shells["kbyr_sub"] = sub(shells["kbyr"],1:k,:);
-  shells["k_sub"] = sub(shells["k"],1:k);
-  shells["k2_sub"] = sub(shells["k2"],1:k);
-  shells["kt_sub"] = sub(shells["kt"],1:k);
+  F[:psijh_sub] = view(F[:psijh],:,1:k);
+  F[:delta_sub] = view(F[:delta],1:k);
+  F[:tauh_sub] = view(F[:tauh],1:k);
+  F[:Plam_sub] = view(F[:Plam],:,1:k);
+  F[:Lambda_sub] = view(F[:Lambda],:,1:k);
+  F[:h2_sub] = view(F[:h2],1:k);
+  F[:scores_sub] = view(F[:scores],1:k,:);
 end
 
 function covfact(A,tol=1e-10)
